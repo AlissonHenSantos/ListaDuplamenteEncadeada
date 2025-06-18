aedfbef97276a995f0042ddf5dd82a84f5022dc1
AlissonHenrique21 <alisson.nascimento@habitis.com.br>
2025-06-17 22:05:22 -0300

    feat: addAtIndex

---
+++ models/Linkedlist.js
@@ -0,0 +1,126 @@
+class LinkedList{
+    #head;
+    #tail;
+    #qtd;
+
+    constructor(){
+        this.#head = null;
+        this.#tail = null;
+        this.#qtd = 0;
+    }
+
+    addFirst(novoDado){
+        const novoNo = new No(novoDado);
+        if(this.#head==null)
+            this.#tail = novoNo;
+        else{
+            novoNo.proximo = this.#head;
+            this.#head.anterior = novoNo;
+        }
+        this.#head = novoNo;
+        this.#qtd++;
+        return true;
+    }
+
+    addLast(novoDado){
+        const novoNo = new No(novoDado);
+        if(this.#head===null)// vazia
+            this.#head = novoNo;
+        else{
+           novoNo.anterior = this.#tail;
+           this.#tail.proximo = novoNo;
+
+        }
+        this.#tail = novoNo;
+        this.#qtd++;
+        return true;
+    }
+
+   addAtIndex(index, dado){
+        const novoNo = new No(dado)
+        if(this.isEmpty() || index <= 1)
+            return this.addFirst(novoNo)
+        if(index > this.#qtd)
+            return this.addLast(novoNo)
+
+        let noAux = this.#head
+        let posicao = 0
+        while(posicao != index - 1){
+            noAux = noAux.proximo
+            posicao++
+        }
+            novoNo.anterior = noAux
+            novoNo.proximo = noAux.proximo
+            novoNo.anterior.proximo = novoNo
+            novoNo.proximo.anterior = novoNo
+
+        this.#qtd++
+   }
+
+    removeFirst(){
+        const dadoRemovido = this.#head.dado;
+        this.#head = this.#head.proximo;
+        if(this.#head!==null)
+            this.#head.anterior = null;
+        else
+            this.#tail = null;
+        this.#qtd--;
+        return dadoRemovido;
+    }
+
+    removeLast(){
+        const dadoRemovido = this.#tail.dado;
+        this.#tail = this.#tail.anterior;
+        if(this.#tail!=null)
+            this.#tail.proximo = null;
+        else
+          this.#head = null;
+        this.#qtd--;
+        return dadoRemovido;
+    }
+    
+    getLast(){
+      return this.#tail.dado;
+    }
+     getFirst(){
+        return this.#head.dado;
+     }
+    isEmpty(){
+        return this.#head === null;
+    }
+
+    get length(){
+        return this.#qtd;
+    }
+
+    //-------------------------------------
+//Quando um objeto tem um iterator, ele pode ser iterado com construções como [ for(const item of minhaLista)*/
+
+[Symbol.iterator]() {         
+    let noAtual = this.#head;
+          return {
+            next: function() {
+              if (noAtual!==null) {
+                let valor = noAtual.dado;
+                noAtual = noAtual.proximo;
+                return { value: valor, done: false };
+              } else {
+                return { done: true };
+              }
+            }
+          };
+        }
+  //—----------------
+    toString() {
+          let result = "";
+          let noAtual = this.#head;
+          while (noAtual !== null) {
+              result += noAtual;
+              noAtual = noAtual.proximo;
+          }
+          return result;
+      }
+   //----------------  
+
+   
+    }
